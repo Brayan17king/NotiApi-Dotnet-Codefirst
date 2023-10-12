8edb498fb998aad079f5a6be3fe9e367310f0f2c
Brayan17king <xbrayansneyder@gmail.com>
2023-10-12 12:55:37 -0500

    feat: :sparkles: Creation of Mapping for the Dtos

---
+++ API/Profiles/MappingProfiles.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+
+namespace API.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        protected MappingProfiles()
+        {
+            CreateMap<Auditoria, AuditoriaDto>().ReverseMap();
+            CreateMap<Blockchain, BlockchainDto>().ReverseMap();
+            CreateMap<EstadoNotificacion, EstadoNotificacionDto>().ReverseMap();
+            CreateMap<Formato, FormatoDto>().ReverseMap();
+            CreateMap<GenericovsSubmodulo, GenericovsSubmoduloDto>().ReverseMap();
+            CreateMap<HiloRespuestaNotificacion, HiloRespuestaNotificacionDto>().ReverseMap();
+            CreateMap<MaestrovsSubmodulo, MaestrovsSubmoduloDto>().ReverseMap();
+            CreateMap<ModuloMaestro, ModuloMaestroDto>().ReverseMap();
+            CreateMap<ModuloNotificacion, ModuloNotificacionDto>().ReverseMap();
+            CreateMap<PermisosGenerico, PermisosGenericoDto>().ReverseMap();
+            CreateMap<Radicado, RadicadoDto>().ReverseMap();
+            CreateMap<Rol, RolDto>().ReverseMap();
+            CreateMap<RolvsMaestro, RolvsMaestroDto>().ReverseMap();
+            CreateMap<Submodulo, SubmoduloDto>().ReverseMap();
+            CreateMap<TipoNotificacion, TipoNotificacionDto>().ReverseMap();
+            CreateMap<TipoRequerimiento, TipoRequerimientoDto>().ReverseMap();
+
+
+        }
+    }
+}
