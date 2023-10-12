6bcdf2e0b6af34bb8b984b0d9448aaab301e3e77
Brayan17king <xbrayansneyder@gmail.com>
2023-10-12 12:52:35 -0500

    feat: :memo: Update Entity

--- Core/Entities/Blockchain.cs
+++ Core/Entities/Blockchain.cs
@@ -10,8 +10,8 @@ public class Blockchain : BaseEntity
     public string HasGenerado { get; set; }
     public int IdHiloRespuestaNotificacion { get; set; }
     public HiloRespuestaNotificacion HiloRespuestaNotificaciones { get; set; }
-   public int IdAuditoria { get; set; }
-   public Auditoria Auditorias { get; set; } 
-   public int IdTipoNotificacion { get; set; }
-   public TipoNotificacion TipoNotificaciones { get; set; }
+    public int IdAuditoria { get; set; }
+    public Auditoria Auditorias { get; set; }
+    public int IdTipoNotificacion { get; set; }
+    public TipoNotificacion TipoNotificaciones { get; set; }
 }
